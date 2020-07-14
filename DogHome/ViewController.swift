a983483cf9efbc673367a537a6b4f151a25429ce
Pavel Vaitsikhouski <pavel.vaitsikhouski@schibsted.com>
2020-07-14 11:36:58 +0300

    Initial Commit

---
+++ DogHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogHome
+//
+//  Created by Pavel Vaitsikhouski on 7/14/20.
+//  Copyright © 2020 vpaveldm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
