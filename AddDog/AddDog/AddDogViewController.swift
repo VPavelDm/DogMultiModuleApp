4cae7364e60d51b333187b4934b7d821d2db7654
Pavel Vaitsikhouski <pavel.vaitsikhouski@schibsted.com>
2020-07-15 16:50:23 +0300

    Added add dog module

---
+++ AddDog/AddDog/AddDogViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddDogViewController.swift
+//  AddDog
+//
+//  Created by Pavel Vaitsikhouski on 7/15/20.
+//  Copyright © 2020 vpaveldm. All rights reserved.
+//
+
+import UIKit
+
+class AddDogViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
